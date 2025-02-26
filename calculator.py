0694566b3ce3300d94f3c4644a260a7b80d375b7
Mohaa922 <148335377+Mohaa922@users.noreply.github.com>
2025-02-26 13:49:26 +0100

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,11 @@
+import math
+
+def calculate_perimeter(radius: float) -> float:
+    """
+    Calcule le périmètre d'un cercle à partir de son rayon.
+    :param radius: Rayon du cercle (float)
+    :return: Périmètre du cercle (float)
+    """
+    if radius < 0:
+        raise ValueError("Le rayon ne peut pas être négatif")
+    return 2 * math.pi * radius
